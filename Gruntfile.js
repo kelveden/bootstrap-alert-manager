1987a31eaac18e79885ee56db4817b42ecc14a73
Alistair Dutton <alistair.dutton@nokia.com>
2013-09-25 22:08:05 +0100

    Ensure that bumping commits all relevant files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,6 +54,7 @@ module.exports = function (grunt) {
         bump: {
             options: {
                 files: [ 'package.json', 'bower.json' ],
+                commitFiles: [ 'package.json', 'bower.json' ],
                 pushTo: "origin"
             }
         }
