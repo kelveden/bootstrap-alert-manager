d2436edf3b68d7c5fee7140a32389041c90e1e17
Alistair Dutton <alistair.dutton@nokia.com>
2013-09-25 22:00:09 +0100

    Bump version in bower.json too.

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,6 +53,7 @@ module.exports = function (grunt) {
         },
         bump: {
             options: {
+                files: [ 'package.json', 'bower.json' ],
                 pushTo: "origin"
             }
         }
